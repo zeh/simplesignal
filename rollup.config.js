7f89280517c7bc47fdd7e3ff486856e5b4150183
Zeh Fernando <zeh@work.co>
2018-12-14 10:45:53 -0500

    Chore: don't generate source maps anymore

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,10 @@ import pkg from "./package.json";
 export default {
 	input: "src/SimpleSignal.ts",
 	output: [
-		{ name: "simplesignal", sourcemap: true, file: pkg.module, format: "es" },
+		{ name: "simplesignal", sourcemap: false, file: pkg.module, format: "es" },
 		{
 			name: "SimpleSignal",
-			sourcemap: true,
+			sourcemap: false,
 			file: pkg.main,
 			format: "umd",
 			footer: "module.exports.default = module.exports; // Terrible injection just so it works regardless of how it's required\n"
